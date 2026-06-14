78ee09d15ee15db7b758a2563d181e7aeeaefebb
HeQiTZD <sflores35megub@gmail.com>
2026-06-14 15:37:20 +0800

    feat: add event filter and button slot declarations to ConfigInitDialog

--- UI/ConfigInit/configinitdialog.h
+++ UI/ConfigInit/configinitdialog.h
@@ -2,6 +2,7 @@
 #define CONFIGINITDIALOG_H
 
 #include <QDialog>
+#include <QPoint>
 
 namespace Ui {
 class ConfigInitDialog;
@@ -15,14 +16,25 @@ public:
     explicit ConfigInitDialog(QWidget *parent = nullptr);
     ~ConfigInitDialog();
 
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
 private slots:
     void on_btnSubmit_clicked();
     void on_btnCancel_clicked();
+    void on_btnMinimize_clicked();
+    void on_btnClose_clicked();
 
 private:
     Ui::ConfigInitDialog *ui;
+    bool m_dragging;
+    QPoint m_dragPosition;
+
     bool validateInputs();
     void saveConfig();
+    void setupWindowFlags();
+    void setupShadow();
+    void setupTitleBar();
 };
 
 #endif // CONFIGINITDIALOG_H
