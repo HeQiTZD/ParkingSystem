2afeab00e6a914a2bbee7bd6ad8dc3887c2d9997
HeQiTZD <sflores35megub@gmail.com>
2026-06-17 14:55:13 +0800

    登录界面美化

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "UI/MainWindow/mainwindow.h"
 #include "UI/Login/logindialog.h"
 #include <QApplication>
-
+#include <QStyleFactory>
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     LoginDialog loginDialog;
+    //loginDialog.setWindowFlags(Qt::FramelessWindowHint);
     loginDialog.exec();
 
     return app.exec();
