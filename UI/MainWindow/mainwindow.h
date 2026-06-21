1411caf9a9c1609c33ce86e55129862cd338a3e7
HeQiTZD <sflores35megub@gmail.com>
2026-06-21 09:57:14 +0800

    feat: 恢复窗口拖动和拉伸功能

--- UI/MainWindow/mainwindow.h
+++ UI/MainWindow/mainwindow.h
@@ -1,7 +1,8 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "circleprogress.h"
 #include <QMainWindow>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,17 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    bool nativeEvent(const QByteArray &eventType, void *message, qintptr *result) override;
+
+private slots:
+    void updateTime();
+
 private:
     Ui::MainWindow *ui;
+    QTimer *m_timer;
+    CircleProgress *m_parkingChart;
+    int m_borderWidth = 8;  // 窗口边缘拉伸感应区域宽度
 };
 
 #endif // MAINWINDOW_H
