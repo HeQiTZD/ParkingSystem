7d158744f8286e7bc99dd9a592ddd704e938566a
HeQiTZD <sflores35megub@gmail.com>
2026-07-19 23:56:42 +0800

    refactor(UserManager): 移除内联表格UI逻辑，精简用户管理组件
    - 移除 setupTable/populateTable 等表格展示代码
    - 移除分页、搜索、空状态等交互逻辑
    - 清理不再需要的头文件和成员变量
    - 调整 QSS 表格内边距

--- UI/UserManager/usermanagement.h
+++ UI/UserManager/usermanagement.h
@@ -2,17 +2,12 @@
 #define USERMANAGEMENT_H
 
 #include <QWidget>
-#include <QList>
-#include <QVariant>
 
 namespace Ui {
 class UserManagement;
 }
 
 class DatabaseManager;
-class QTableWidget;
-class QLabel;
-class PaginationWidget;
 
 class UserManagement : public QWidget
 {
@@ -23,24 +18,8 @@ public:
     ~UserManagement();
 
 private:
-    void setupTable();
-    void populateTable();
-    void showEmptyState();
-    void hideEmptyState();
-    QString formatPhone(const QString &phone);
-
-private slots:
-    void onSearchClicked();
-    void onPageChanged(int page);
-
-private:
     Ui::UserManagement *ui;
-    QTableWidget *m_tableWidget = nullptr;
-    PaginationWidget *m_pagination = nullptr;
-    QWidget *m_emptyStateWidget = nullptr;
-    QLabel *m_recordCountLabel = nullptr;
     DatabaseManager *m_db = nullptr;
-    QList<QVariantList> m_allData;
 };
 
 #endif // USERMANAGEMENT_H
